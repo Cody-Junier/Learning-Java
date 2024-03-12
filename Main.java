e9e18b01d4fc743839ab98bb877459ad9e544e31
cody-junier <codyjunier@gmail.com>
2024-03-12 15:38:01 -0500

    Playing around with print lines, addition, etc

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.print("Is this on the same line? ");
+        System.out.println("No, it's not");
+        System.out.println(28);
+        System.out.println(28 + 28);
     }
 }
