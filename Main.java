31695bc54cc067b6293247407294b849a90095ff
cody-junier <codyjunier@gmail.com>
2024-03-12 16:05:03 -0500

    Working with variables, getting used to proper syntax

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        final String hello = "Hello World!";
+        int perfect = 28;
+        int x = 1, y = 5, z= 20; 
+        float j = 3.14f;
+        perfect = 28 + 28;
+        System.out.println(hello);
         System.out.print("Is this on the same line? ");
         System.out.println("No, it's not");
-        System.out.println(28);
-        System.out.println(28 + 28);
+        System.out.println(perfect);
+        System.out.println(perfect + perfect); //single line comment
+       /* multi-line
+        comment */
+        System.out.println(hello + " What is "+ j + " times " + z + "? It's "+ j*z + "!");
     }
 }
